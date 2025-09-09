cf57628aae8e59db61ad70f95bd3fefb8257b932
Esraa Hassouna <esraa.hassouna9.11@gmail.com>
2025-09-09 15:20:50 +0300

    loop for removing not allowed first name students

--- Python Projects/basics/OOP fundmentals.py
+++ Python Projects/basics/OOP fundmentals.py
@@ -1,10 +1,12 @@
 # OOP
 # instance attributes and methods
-# class attributes and methods can be accessd inside constructor and instance methods using self keyword
+# class attributes and methods can be accessd inside constructor
+# and instance methods using class name
+
 class Student:
     
-    notAllowedNames= ["admin","user","null"]
-    deleteUser=0
+    notAllowedNames= ["admin","user","null"]# any first name in this list is not allowed
+    users=0 # to count how many users 
 
     def __init__(self,fName,lName,gender,age):
         self.fName=fName
@@ -12,13 +14,20 @@ class Student:
         self.gen=gender
         self.age=age
         self.notAllowedNames=fName
-        Student.deleteUser+=1
+#class attribute accessed all over the class using class name
+        Student.users+=1
 
+# **deleteUser**
+# if it is instance attribute there will be no change
+# remains 0
+# if it is class attribute it will be changed (changble through all over the class methods)  
     
     def removeNotAllowedNames(self):
         if self.fName in Student.notAllowedNames:
-            Student.deleteUser-=1
-        return f'{self.fName} is not allowed name, user deleted'        
+            Student.users-=1
+            return f'{self.fName} is not allowed name, user deleted'
+        else:
+            return f'{self.fName} is allowed name, user added'        
 
     
     def fullName(self):
@@ -29,25 +38,25 @@ class Student:
 
     def allInfo(self):
         if self.IsAdult():
-            return f"Adult, then your full name is {self.fullName()} and you are {self.gen}"
+            return f"Adult, your full name is {self.fullName()} and you are {self.gen}"
         elif not self.IsAdult():
             return f"{self.fName}, you are under age under your dad's supervision, {self.lName}"
 
-
-
-
-print(Student.deleteUser)
+print("number of students",Student.users) # 0 there are no objects (users)
 
 stu1= Student("Esraa","Hassouna","female",22)
 stu2= Student("Omar","Mohamed","Male",24)
 stu3= Student("Mariam","Omar","Female",2)
 stu4= Student("null","catroon","Female",95)
+allStudents=[stu1,stu2,stu3,stu4]
 
-print(Student.deleteUser)
 
-print(Student.removeNotAllowedNames())
+print("number of students",Student.users)# 4 objects are created from Student class 
 
-print(Student.deleteUser)
+for student in allStudents:
+    print(student.removeNotAllowedNames())
+
+print("number of students",Student.users)# 1of the usres are deleted so there are now 3 usres only
         
 print(stu1.allInfo())
 print(stu2.allInfo())
